c14678bffaf00ce1edc90790509293c2fd09a5c8
Giovani Montantes <gio@MacBook-Pro.local>
2026-06-24 23:37:06 -0500

    Port workspace and front app modules to AeroSpace

--- plugins/front_app.sh
+++ plugins/front_app.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-case "$SENDER" in
-"front_app_switched")
-	sketchybar --set "$NAME" label="$INFO"
-	;;
-esac
+app_name="$INFO"
+
+if [ -z "$app_name" ] && command -v aerospace >/dev/null 2>&1; then
+	app_name="$(aerospace list-windows --focused --format '%{app-name}' 2>/dev/null)"
+fi
+
+[ -z "$app_name" ] && exit 0
+
+sketchybar --set "$NAME" label="$app_name"
